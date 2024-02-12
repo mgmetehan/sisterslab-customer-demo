50d0f56ebb108aa2ec92fcb7beb79a8757aa7e05
mgmetehan <mgmetehan@hotmail.com>
2024-02-12 18:56:13 +0300

    init project

---
+++ src/main/java/com/sisterslab/sisterslabcustomer/SisterslabCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.sisterslab.sisterslabcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisterslabCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisterslabCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sisterslab/sisterslabcustomer/SisterslabCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisterslab.sisterslabcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisterslabCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
