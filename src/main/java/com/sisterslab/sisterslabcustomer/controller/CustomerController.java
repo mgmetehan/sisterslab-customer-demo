8d94796877f721bb7913da16a754173dfd5508f5
mgmetehan <mgmetehan@hotmail.com>
2024-02-12 20:25:58 +0300

    add getAll & getFindById methods

--- src/main/java/com/sisterslab/sisterslabcustomer/controller/CustomerController.java
+++ src/main/java/com/sisterslab/sisterslabcustomer/controller/CustomerController.java
@@ -3,10 +3,15 @@ package com.sisterslab.sisterslabcustomer.controller;
 import com.sisterslab.sisterslabcustomer.model.Customer;
 import com.sisterslab.sisterslabcustomer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -17,7 +22,19 @@ public class CustomerController {
     @PostMapping()
     public Customer addCustomer(@RequestBody Customer customer) {
         return customerService.saveCustomer(customer);
-
     }
+
+    @GetMapping()
+    public List<Customer> allCustomer() {
+        return customerService.allCustomer();
+    }
+
+    @GetMapping("{id}")
+    public Customer getCustomerById(@PathVariable("id") Long id) {
+        return customerService.getCustomerFindById(id);
+    }
+
+
+
 
 }
--- src/main/java/com/sisterslab/sisterslabcustomer/service/CustomerService.java
+++ src/main/java/com/sisterslab/sisterslabcustomer/service/CustomerService.java
@@ -3,14 +3,35 @@ package com.sisterslab.sisterslabcustomer.service;
 import com.sisterslab.sisterslabcustomer.model.Customer;
 import com.sisterslab.sisterslabcustomer.respository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerService {
     private final CustomerRepository customerRepository;
 
     public Customer saveCustomer(Customer customer) {
         return customerRepository.save(customer);
     }
+
+    public List<Customer> allCustomer() {
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerFindById(Long id) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        if (customer.isPresent()) {
+            return customer.get();
+        } else {
+            log.info("Aradiginiz id de kullanici yok!!!!");
+            return null;
+        }
+    }
+
+
 }
